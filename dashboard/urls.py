6aa9df3872ad797f661979a68d856f6422a1901e
Kris Han <sghantb@163.com>
2026-03-17 03:15:26 +0000

    hotfix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import dashboard_list, task_list
 
 urlpatterns = [
-    path('dashboard_list/', dashboard_list),
-    path('task_list/', task_list),
+    path('dashboard_list', dashboard_list),
+    path('task_list', task_list),
 
 ]
